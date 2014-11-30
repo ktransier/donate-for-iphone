6acad9f29b555f74e323f247fefb402c23f54f0e
ktransier <ktransier@gmail.com>
2014-11-30 14:03:59 -0800

    org table view controller clean up

--- StripeTestObjC/OrganizationsTableViewController.h
+++ StripeTestObjC/OrganizationsTableViewController.h
@@ -1,6 +1,6 @@
 //
 //  OrganizationsTableViewController.h
-//  StripeTestObjC
+//  Together
 //
 //  Created by Kenneth Transier on 11/14/14.
 //  Copyright (c) 2014 Kenneth Transier. All rights reserved.
@@ -9,6 +9,4 @@
 #import <UIKit/UIKit.h>
 
 @interface OrganizationsTableViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
-
-
 @end
